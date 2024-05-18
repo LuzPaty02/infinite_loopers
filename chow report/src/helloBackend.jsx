aeaee9847b522a36d6103cb28f75946d0fe3a922
LuzPaty02 <luzpatyhr@gmail.com>
2024-05-18 13:47:58 -0600

    conexion backend y frontend uwuwu

---
+++ chow report/src/helloBackend.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const HelloBackend = () => {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/');
+        console.log('Response data:', response.data); // Log response data
+        setMessage(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
+
+  return (
+    <div>
+      <p>Message from Flask backend: {message}</p>
+    </div>
+  );
+};
+
+export default HelloBackend;
