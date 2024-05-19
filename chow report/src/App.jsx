18dfe253ac346353d8a715a1fd84c9afad2a2010
ZAnaKarenZ <117191903+ZAnaKarenZ@users.noreply.github.com>
2024-05-19 01:41:49 -0600

    Creo que ya

--- chow report/src/App.jsx
+++ chow report/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import UserReport from './user/UserReport.jsx'
-import GenerateReport from './user/GenerateReport'
+import GenerateReport from './user/GenerateReport.jsx'
+import UserPage from './user/UserPage.jsx'
+import UserProfile from './user/UserProfile.jsx'
 
 function App () {
     return (
         <Routes>
+                <Route path="/user-page" element={<UserPage />} />
+                <Route path="/user-profile" element={<UserProfile />} />
                 <Route path="/user-report" element={<UserReport />} />
                 <Route path="/generate-report" element={<GenerateReport />} />
         </Routes>
--- chow report/src/user/UserProfile.jsx
+++ chow report/src/user/UserProfile.jsx
@@ -1,17 +1,15 @@
-
-import { Avatar } from "flowbite-react";
+import { FaUser } from 'react-icons/fa';
 
 const UserProfile = () => {
     return (
         <>
             <div className="justify-center flex flex-col items-center min-h-screen bg-origin-border h-14 bg-gradient-to-bl from-blue-50 to-indigo-300">
+                <div className="text-4xl">
+                    <FaUser />
+                </div>
                 <header className="px-4 py-10">
                     <h1 className="font-mono text-xl">USER PROFILE</h1>
                 </header>
-                <div>
-                    <Avatar />
-                    {/* You can pass props to Avatar component if needed */}
-                </div>
                 <div>
                     <p>nombre</p>
                 </div>
