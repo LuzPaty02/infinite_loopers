4e9596a8eb07fc187a07fd6214f8786316dfff3d
AdelaSolorio <a01637205@tec.mx>
2024-05-19 02:45:17 -0600

    added routes to signup

--- chow report/src/App.jsx
+++ chow report/src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Login from "./ong/login"
 import SignupONG from "./ong/signupONG"
+import SignupUsers from "./ong/signupUsers"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignupONG />} />
+        <Route path="/signupONG" element={<SignupONG />} />
+        <Route path="/signupUsers" element={<SignupUsers />} />
+
       </Routes>
     </>
   )
